836ccb78838c4d56a3a03f9c8bf965f1d8bb1af3
Victor Persson <vicper-4@student.ltu.se>
2017-02-04 14:31:41 +0100

    Add some of javadac

--- src/Sim/LossyLink.java
+++ src/Sim/LossyLink.java
@@ -1,12 +1,19 @@
 package Sim;
 
-// This implements a link with loss, jitter and delay
+/**
+ * This implements a link with loss, jitter and delay.
+ */
 
 public class LossyLink extends Link{
     private int delay;
     private float jitter;
     private float droprate;
 
+    /**
+     * @param delay     Avrage delay of packets passing thru this link.
+     * @param jitter    Jitter of the link.
+     * @param droprate  Droprate of the link as probability from 0.0 - 1.0.
+     */
     public LossyLink(int delay, float jitter, float droprate)
     {
         super();
@@ -15,6 +22,11 @@ public class LossyLink extends Link{
         this.droprate = droprate;
     }
 
+    /**
+     * Called when a message enters the link, infers delay, jitter and droprate
+     * @param src   Message sender.
+     * @param ev    Event. 
+     */
     @Override
     public void recv(SimEnt src, Event ev)
     {
