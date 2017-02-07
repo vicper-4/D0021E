5220de9eb6620d4e2f27fe312add56cd559eea11
nikalas <nikalas@users.noreply.github.com>
2017-02-07 13:19:27 +0100

    fix trailing characters

--- src/Sim/Run.java
+++ src/Sim/Run.java
@@ -8,7 +8,7 @@ public class Run {
  		//Creates two links
  		Link link1 = new LossyLink(5.0f,0.5f,0.2f);
 		LossyLink link2 = new LossyLink(10.0f,0.5f,0.2f);
-		
+
 		// Create two end hosts that will be
 		// communicating via the router
 		Node host1 = new Node(1,1);
@@ -19,23 +19,23 @@ public class Run {
 		host2.setPeer(link2);
 
 		// Creates as router and connect
-		// links to it. Information about 
+		// links to it. Information about
 		// the host connected to the other
 		// side of the link is also provided
 		// Note. A switch is created in same way using the Switch class
 		Router routeNode = new Router(2);
 		routeNode.connectInterface(0, link1, host1);
 		routeNode.connectInterface(1, link2, host2);
-		
+
 		// Generate some traffic
 		// host1 will send 3 messages with time interval 5 to network 2, node 1. Sequence starts with number 1
-		host1.StartSending(2, 2, 3, 5, 1); 
+		host1.StartSending(2, 2, 3, 5, 1);
 		// host2 will send 2 messages with time interval 10 to network 1, node 1. Sequence starts with number 10
-		host2.StartSending(1, 1, 2, 10, 10); 
-		
+		host2.StartSending(1, 1, 2, 10, 10);
+
 		// Start the simulation engine and of we go!
 		Thread t=new Thread(SimEngine.instance());
-	
+
 		t.start();
 		try
 		{
@@ -44,9 +44,6 @@ public class Run {
 		catch (Exception e)
 		{
 			System.out.println("The motor seems to have a problem, time for service?");
-		}		
-
-
-
+		}
 	}
 }
