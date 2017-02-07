fa3b913e5dbc76e0617eedf3364bf1348def2d3f
nikalas <nikalas@users.noreply.github.com>
2017-02-07 10:06:25 +0100

    Fix formating of tabs, spaces and trailing characters

--- src/Sim/LossyLink.java
+++ src/Sim/LossyLink.java
@@ -6,54 +6,53 @@ import java.util.Random;
  * This implements a link with loss, jitter and delay.
  */
 public class LossyLink extends Link{
-    private float delay;
-    private float jitter;
-    private float droprate;
+	private float delay;
+	private float jitter;
+	private float droprate;
 
-    /**
-     * @param delay     Avrage delay of message passing thru this link. (ms)
-     * @param jitter    Jitter of the link. Random uniform distribution. (ms)
-     * @param droprate  Droprate of the link as probability from 0.0 - 1.0. 
-     */
-    public LossyLink(float delay, float jitter, float droprate)
-    {
-        super();
-        this.delay = delay;
-        this.jitter = jitter;
-        this.droprate = droprate;
-    }
+	/**
+	 * @param delay		Avrage delay of message passing thru this link. (ms)
+	 * @param jitter	Jitter of the link. Random uniform distribution. (ms)
+	 * @param droprate	Droprate of the link as probability from 0.0 - 1.0.
+	 */
+	public LossyLink(float delay, float jitter, float droprate)
+	{
+		super();
+		this.delay = delay;
+		this.jitter = jitter;
+		this.droprate = droprate;
+	}
 
-    /**
-     * Called when a message enters the link.\
-     * Infers delay, jitter and droprate
-     * @param src   Message sender.
-     * @param ev    Event. 
-     */
-    @Override
-    public void recv(SimEnt src, Event ev)
-    {
-        if (ev instanceof Message)
-        {
-            Random random = new Random();
-            float dt = (random.nextFloat() * 2 - 1) * jitter;
-            double wait = delay + dt;
-            if (1.0f - random.nextFloat() > droprate)
-            {
-                System.out.println("Link recv msg, passes it through");
-			    if (src == _connectorA)
-			    {
-			    	send(_connectorB, ev, wait);
-			    }
-			    else
-			    {
-			    	send(_connectorA, ev, wait);
-			    }
-            }
-            else
-            {
-                System.out.println("Link recv msg, drops it");
-            }
-        }
-    }
+	/**
+	 * Called when a message enters the link.\
+	 * Infers delay, jitter and droprate
+	 * @param src	Message sender.
+	 * @param ev	Event.
+	 */
+	@Override
+	public void recv(SimEnt src, Event ev)
+	{
+		if (ev instanceof Message)
+		{
+			Random random = new Random();
+			float dt = (random.nextFloat() * 2 - 1) * jitter;
+			double wait = delay + dt;
+			if (1.0f - random.nextFloat() > droprate)
+			{
+				System.out.println("Link recv msg, passes it through");
+				if (src == _connectorA)
+				{
+					send(_connectorB, ev, wait);
+				}
+				else
+				{
+					send(_connectorA, ev, wait);
+				}
+			}
+			else
+			{
+				System.out.println("Link recv msg, drops it");
+			}
+		}
+	}
 }
-
