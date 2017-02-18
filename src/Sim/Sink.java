b4fc246356cb305cef5092d56a65be3f1ce58945
nikalas <n_eliason@hotmail.com>
2017-02-18 22:38:05 +0100

    fix spelling errors

--- src/Sim/Sink.java
+++ src/Sim/Sink.java
@@ -1,11 +1,11 @@
 package Sim;
 
 /**
- * Class for receving/consuming network trafic
+ * Class for receiving/consuming network traffic
  */
 public class Sink
 {
-    private long recived;
+    private long received;
     private double last;
     private double avgrPeriod;
     private double period;
@@ -22,8 +22,8 @@ public class Sink
         return avgrPeriod;
     }
 
-    public long getRecived() {
-        return recived;
+    public long getReceived() {
+        return received;
     }
 
     public double getPeriod() {
@@ -68,24 +68,24 @@ public class Sink
     }
 
     /**
-     * handles reciving of messages/trafic.
-     * @param ev        Incomming message/trafic
+     * handles receiving of messages/traffic.
+     * @param ev        Incoming message/traffic
      * @param currTime  Current time.
      */
     public void recv(Message ev, double currTime)
     {
         calcStat(currTime - ev.timeSent, currTime);
-        
+
         last = currTime;
     }
 
     /**
      * Calls the other methods that calculate statistics.
-     * @param tt        Transit time of the recived package.
+     * @param tt        Transit time of the received package.
      * @param currTime  Current time.
      */
     private void calcStat(double tt, double currTime) {
-        recived(1);
+        received(1);
         period(currTime);
         deviation();
         delay(tt);
@@ -93,37 +93,37 @@ public class Sink
     }
 
     /**
-     * Increments the number of recived packages.
+     * Increments the number of received packages.
      * @param n Number to increment by.
      */
-    private void recived(long n) {
-        recived += n;
+    private void received(long n) {
+        received += n;
     }
 
     /**
-     * calculated period of the message and updates the avrage period.
+     * calculated period of the message and updates the average period.
      * @param currTime  Current time.
      */
     private void period(double currTime) {
         period = currTime - last;
-        
+
         if(avgrPeriod == 0) {avgrPeriod = period;}
 
-        avgrPeriod -= avgrPeriod/recived;
-        avgrPeriod += period/recived;
+        avgrPeriod -= avgrPeriod/ received;
+        avgrPeriod += period/ received;
     }
 
     /**
-     * calculated deviation in period of the message and updates the avrage period deviation.
+     * calculated deviation in period of the message and updates the average period deviation.
      */
     private void deviation() {
         periodDeviation = period - avgrPeriod;
 
         if(avgrPeriodDeviation == 0) {avgrPeriodDeviation = periodDeviation;}
 
-        avgrPeriodDeviation -= avgrPeriodDeviation/recived;
-        avgrPeriodDeviation += Math.abs(periodDeviation)/recived;
-        
+        avgrPeriodDeviation -= avgrPeriodDeviation/ received;
+        avgrPeriodDeviation += Math.abs(periodDeviation)/ received;
+
         //TODO consider not using hardcoded weight for new values.
         if(periodDeviation > 0)
         {
@@ -138,30 +138,30 @@ public class Sink
     }
 
     /**
-     * calculated jitter of the message and updates the avrage jitter.
+     * calculated jitter of the message and updates the average jitter.
      * @param tt  Transit time.
      */
     private void jitter(double tt) {
         jitter = Math.abs(delay - tt);
-		avgrJitter += (1.0 / ((double) recived)) * (Math.abs(tt - avgrDelay) - avgrJitter);
+		avgrJitter += (1.0 / ((double) received)) * (Math.abs(tt - avgrDelay) - avgrJitter);
     }
 
     /**
-     * calculated period of the message and updates the avrage period.
+     * calculated period of the message and updates the average period.
      * @param tt  Transit time.
      */
     private void delay(double tt) {
         delay = tt;
-        avgrDelay -= avgrDelay/recived;
-        avgrDelay += delay/recived;
+        avgrDelay -= avgrDelay/ received;
+        avgrDelay += delay/ received;
     }
 
     /**
-     * Resets all gatherd statistics.
+     * Resets all gathered statistics.
      */
     private void reset()
     {
-        recived = 0;
+        received = 0;
         last = 0.0;
         avgrPeriod = 0.0;
         period = 0.0;
