1c4ec2eafe832506e3299251e72b5faf3f16ae46
nikalas <n_eliason@hotmail.com>
2017-02-24 00:43:46 +0100

    sexifying stuff

--- src/Sim/MoveEnt.java
+++ src/Sim/MoveEnt.java
@@ -4,14 +4,14 @@ package Sim;
 // fields in the message are who the sender is, the destination and a sequence 
 // number
 
-public class Message implements Event{
+public class MoveEnt implements Event{
 	private SimEnt _link;
 	private NetworkAddr _newNetwork;
 	
-	Message (SimEnt _link, NetworkAddr newNetwork)
+	MoveEnt (SimEnt _link, NetworkAddr newNetwork)
 	{
-		this.link = _link;
-		this.newNet = newNetwork;
+		this._link = _link;
+		this._newNetwork = newNetwork;
 	}
 
 	public SimEnt link()
@@ -19,9 +19,9 @@ public class Message implements Event{
 		return _link;
 	}
 
-	public networkId()
+	public int networkId()
 	{
-		return _newNetwork;
+		return _newNetwork.networkId();
 	}
 	
 	public void entering(SimEnt locale)
