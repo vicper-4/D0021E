d0de36cf54f5e666aaf6b3cd25d7d1429948356e
nikalas <n_eliason@hotmail.com>
2017-03-03 03:57:22 +0100

    Set up with router1(node1,node2,null) <---> router2(node3) where node node2 moves interfaces on router1

--- src/Sim/Run.java
+++ src/Sim/Run.java
@@ -7,38 +7,50 @@ public class Run {
 	{
  		//Creates two links
 		Link link1 = new Link();
-		//Link link2 = new Link();
+		Link link2 = new Link();
+		Link link3 = new Link();
+		Link link4 = new Link();
+
 		//Link link1 = new LossyLink(1.5f,0.2f,0.05f);
-		Link link2 = new LossyLink(2.0f, 0.1f, 0.08f);
+		//Link link2 = new LossyLink(2.0f, 0.1f, 0.08f);
 	
 		Sink sink1 = new Sink();
 		Sink sink2 = new Sink();
+		Sink sink3 = new Sink();
 		// Create two end hosts that will be
 		// communicating via the router
 		Node host1 = new Node(1,1, sink1);
 		Node host2 = new Node(2,1, sink2);
+		Node host3 = new Node(3,1, sink3);
 
 		//Connect links to hosts
 		host1.setPeer(link1);
 		host2.setPeer(link2);
+		host3.setPeer(link3);
 
 		// Creates as router and connect
 		// links to it. Information about
 		// the host connected to the other
 		// side of the link is also provided
 		// Note. A switch is created in same way using the Switch class
-		Router routeNode = new Router(3);
+		Router routeNode = new Router(4);
+		Router router2 = new Router(2);
 		routeNode.connectInterface(0, link1, host1);
 		routeNode.connectInterface(1, link2, host2);
+
+		// Connect the two routers
+		routeNode.connectInterface(3, link3, router2);
+		router2.connectInterface(0, link3, routeNode);
+		router2.connectInterface(1, link4, host3);
+
+
 
 		// Generate some traffic
 		Generator gen1 = new ConstantGenerator(5);
 		Generator gen2 = new GaussianGenerator(4, 1);
 		Generator gen3 = new PoissonGenerator(5);
-		// host1 will send 500 messages with time interval 5 to network 2, node 1. Sequence starts with number 1000
-		host2.StartSending(1, 1, 25, gen2, 2000); 
-		// host2 will send 100 messages with time interval 10 to network 1, node 1. Sequence starts with number 2000
-		host1.StartSending(2, 1, 20, gen3, 1000);
+		host1.StartSending(2, 1, 20, gen1, 1000);
+		host3.StartSending(2, 1, 20, gen1, 3000);
 
 		//Event disConEv1 = new DisconnectEnt(link2, host2);
 		Event disConEv2 = new DisconnectEnt(link2, routeNode);
@@ -47,7 +59,7 @@ public class Run {
 		//link2.send(link2, disConEv1, 20);
 		link2.send(link2, disConEv2, 35);
 		//link2.send(link2, conEv1, 50);
-		link2.send(link2, conEv2, 45);
+		link2.send(link2, conEv2, 65);
 
 		// Start the simulation engine and of we go!
 		Thread t=new Thread(SimEngine.instance());
