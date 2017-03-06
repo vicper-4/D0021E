82812bbe03bd698f80f1a383d0089c83f2244711
nikalas <n_eliason@hotmail.com>
2017-03-06 15:36:17 +0100

    move the sending of regReqs to update_id(), add javadoc comments and clean up code

--- src/Sim/Node.java
+++ src/Sim/Node.java
@@ -9,9 +9,15 @@ public class Node extends SimEnt {
 	 * Set a new id (network address / ip address)
  	 * @param _id
 	 */
-	public void set_id(NetworkAddr _id) {
+	public void update_id(NetworkAddr _id) {
 		this._deprecated_id = this._id;
 		this._id = _id;
+
+		// Register with HomeAgent
+		if (homeAgent != null)
+		{
+			send(_peer, new RegReq(_id, homeAgent, _seq, SimEngine.getTime()+100.0, SimEngine.getTime()+50.0), 0);
+		}
 	}
 
 	protected NetworkAddr _id;
@@ -23,6 +29,7 @@ public class Node extends SimEnt {
 	private Generator gen;
 	private Sink sink;
 	private boolean _assignedRouter = false;
+	NetworkAddr _localBroadcast = new NetworkAddr(_id.networkId(),0xff);
 
 
 	public Node (int network, int node, Sink sink)
@@ -76,7 +83,7 @@ public class Node extends SimEnt {
 //**********************************************************************************	
 	// Just implemented to generate some traffic for demo.
 	// In one of the labs you will create some traffic generators
-	//TODO look over start sending. Should stuff be moved to generator?
+	//TODO look over startSending (now up). Should stuff be moved to generator?
 	
 	private int _stopSendingAfter = 0; //messages
 	private int _timeBetweenSending = 10; //time between messages
@@ -126,10 +133,14 @@ public class Node extends SimEnt {
 		}
 	}
 
-	// TODO Javadoc
+	/**
+	 * When a timer event is received (i.e. one clock tick) it checks for an assigned router.
+	 * If no router has been assigned it will send a router solicitation, otherwise it will send
+	 * the next message in the queue.
+	 */
 	private void recvTimerEvent()
 	{
-		// TODO check for router handshake
+		// Check for router handshake
 		if (!_assignedRouter) {
 			System.out.printf("%n?? Node %d.%d knows of no router, sends RouterSolicitation\n",_id.networkId(), _id.nodeId());
 			send(_peer, new RouterSolicitation(_id, _broadcast,1), 0);
@@ -153,18 +164,15 @@ public class Node extends SimEnt {
 
 	}
 
+	// TODO JavaDoc
 	private void recvRouterAdvertisement()
 	{
 		System.out.printf("%n!! Node %d.%d received RouterAdvertisement %n",_id.networkId(), _id.nodeId());
+
 		if (!_assignedRouter)
 		{
-			//startSending();
-			
-			send(_peer, new RegReq(_id, homeAgent, _seq, SimEngine.getTime()+100.0, SimEngine.getTime()+50.0), 0);
-		
 			_assignedRouter = true;
 		}
-
 	}
 
 	/** TODO Javadoc
@@ -173,10 +181,10 @@ public class Node extends SimEnt {
 	 */
 	private void recvMsg(Event ev)
 	{
-		// Make calculations
+		// Make time calculations
 		double currTime = SimEngine.getTime();
-		// TODO Should Node still calculate jitter?
 		double tt = currTime - ((Message) ev).timeSent;
+		// TODO Should SimEnt still calculate jitter?
 		calculateJitter(tt);
 
 		sink.recv((Message)ev, currTime);   // Pass message to sink
@@ -190,10 +198,6 @@ public class Node extends SimEnt {
 				currTime,
 				tt);
 
-		// TODO block should probably be removed
-		// Set message sender as target of new messages
-		// recvBindUpdate( ((Message) ev).source() );
-
 		// If message received was sent to deprecated address,
 		// give sender my current address.
 		if (_deprecated_id != null) {
@@ -203,13 +207,19 @@ public class Node extends SimEnt {
 		}
 	}
 
+	/**
+	 * Called when a message that has been redirected from an Home Agent is received.
+	 * @param ev encapsulated message from Home Agent.
+	 */
 	private void recvRedirMsg(Event ev)
 	{
 		send(this, ((RedirMsg) ev).getOriginal(), 0);
 		sendBindUpdate(((Message) ((RedirMsg) ev).getOriginal()).source());
 	}
 
-	// TODO? Javadoc
+	/**
+	 * Prints various statistics about this node.
+	 */
 	public void printStat()
 	{
 		//System.out.printf("Time since last received message: %fms %n", sink.getPeriod());
@@ -259,7 +269,9 @@ public class Node extends SimEnt {
 		sendBindAck();
 	}
 
-	// TODO Javadoc
+	/**
+	 * Should be sent on receiving a BindUpdate
+	 */
 	private void sendBindAck()
 	{
 
