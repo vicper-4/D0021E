76e21ac2b20f722eea4c0f3728eaf3d7d28ac4fb
Victor Persson <vicper-4@student.ltu.se>
2017-03-18 20:01:03 +0100

    add generic message buffer class with maximum size

---
+++ src/Sim/MessageBuffer.java
@@ -0,0 +1,36 @@
+public class MessageBuffer {
+	Message[] buffer;
+	int bufferSize;
+	int buffered;
+
+	MessageBuffer(int bufferSize)
+	{
+		buffer = new Message[bufferSize];
+		this.bufferSize = bufferSize;
+		buffered = 0;
+	}
+
+	void addMsg(Message msg)
+	{
+		if (buffered < bufferSize)
+		{
+			buffer[buffered] = msg;
+			buffered++;
+		}
+		//TODO handle full buffer
+	}
+
+	Message popMsg()
+	{
+		Message first = buffer[0];
+
+		for (int i = 0; (i < buffered) && (i < bufferSize-1); i++;)
+		{
+			buffer[i] = buffer[i+1];
+		}
+
+		buffered--;
+
+		return first;
+	}
+}
